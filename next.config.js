986efb4424df83071dffd56e187f5f7dbf1a8f78
gamemakerr <96029891+gamemakerr@users.noreply.github.com>
2023-02-03 16:37:46 +0900

    redirectrewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "f0972352b2719e2b2dd73ff3096c9af9";
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
